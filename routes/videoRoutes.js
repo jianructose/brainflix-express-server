8c26ba23a9dd7f761d31436f5de55d50f8ae818f
jianructose <70822647+jianructose@users.noreply.github.com>
2024-03-05 04:38:44 +0000

    add images

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid"); // generate a unique id for each video
 
 router.get("/", (req, res) => {
-  console.log("GET /videos");
+  res.send("Welcome to the video routes");
 });
 
 module.exports = router; // export the router
